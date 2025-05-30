f312f1347690395a1755687b9bb1eaa835604b6f
Cory McKiel <cory.mckiel@protonmail.com>
2025-05-29 20:45:20 -0500

    took a test drive

--- src/main.c
+++ src/main.c
@@ -3,6 +3,60 @@
 
 int main(int argc, char **argv)
 {
-    printf("Hello GoogleTest!\n");
+    // Fun test drive for the circular buffer.
+    // Define two messages, one large enough to fit entirely in the buffer,
+    // the other too large.
+    char regular_message[] = "Regular message.\n";
+    char message_with_overwrite[] = "Message with overwrite.\n";
+
+    // Init the buffer to the size of a regular message, plus some extra.
+    circular_buffer_ctx ctx;
+    size_t buff_size = sizeof(regular_message) + 5;
+    if (circular_buffer_init(&ctx, buff_size) == false)
+    {
+        fprintf(stderr, "Buffer init failed!\n");
+        return -1;
+    }
+
+    // Push the regular message into the buffer one byte at a time.
+    for (size_t i = 0; i < sizeof(regular_message); i++)
+    {
+        if (circular_buffer_push(&ctx, regular_message[i]) == false)
+        {
+            fprintf(stderr, "Buffer push failed!\n");
+            return -1;
+        }
+    }
+
+    // Print out the regular message by popping and putting each byte.
+    uint8_t c;
+    while (circular_buffer_pop(&ctx, &c))
+    {
+        putchar(c);
+    }
+
+    // Reinit the buffer to start from scratch.
+    if (circular_buffer_init(&ctx, buff_size) == false)
+    {
+        fprintf(stderr, "Buffer init failed!\n");
+        return -1;
+    }
+
+    // Put the message into the buffer, and expect overwriting to occur.
+    for (size_t i = 0; i < sizeof(message_with_overwrite); i++)
+    {
+        if (circular_buffer_push(&ctx, message_with_overwrite[i]) == false)
+        {
+            fprintf(stderr, "Buffer push failed!\n");
+            return -1;
+        }
+    }
+
+    // Print out the message one byte at a time to demonstrate it was overwritten.
+    while (circular_buffer_pop(&ctx, &c))
+    {
+        putchar(c);
+    }
+
     return 0;
 }
